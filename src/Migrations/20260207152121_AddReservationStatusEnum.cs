fd9cc6348cf7352d4d90c0c030eac488fe6755da
Fathoni <88326103+thoni08@users.noreply.github.com>
2026-02-08 11:12:30 +0700

    feat: add migration for Status column on Reservations table

---
+++ src/Migrations/20260207152121_AddReservationStatusEnum.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResRoomApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReservationStatusEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Reservations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Reservations");
+        }
+    }
+}
